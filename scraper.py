cce556be72dd3a5a43610fdcceb5c8cb75777e2c
ahmed4-glitch455 <mallalahmed4@gmail.com>
2026-03-06 07:48:26 +0100

    Update scraper.py
    Prix < 40.000 DH

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ load_dotenv()
 # --- CONFIGURATION ---
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_API")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
-BASE_URL = "https://www.avito.ma/fr/maroc/voitures_d_occasion-%C3%A0_vendre?price=-60000"
+BASE_URL = "https://www.avito.ma/fr/maroc/voitures_d_occasion-%C3%A0_vendre?price=-40000"
 MAX_PAGES = 30 
 
 def format_price(price_str):
